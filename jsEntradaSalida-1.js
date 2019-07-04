030a1891ec114106a1dbd6e9bafe88a01d526130
EmanuelAl <52534676+EmanuelAl@users.noreply.github.com>
2019-07-04 11:24:14 -0300

    Ejercicios 1 y 2 de js

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,14 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
+function mostrar()
+{
+  var nombre;
+  var confirmar;
+  alert("hola mundo");
+
+  nombre=prompt("ingrese nombre");
+alert(nombre);
+
+confirmar=prompt("ingrese nombre");
+alert(confirmar);
+}
+
