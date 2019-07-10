5e0e785ebe0602df2a2bab8ea69d002ff1b99fb6
EmanuelAl <52534676+EmanuelAl@users.noreply.github.com>
2019-07-09 22:38:42 -0300

    ejercicios E/S (del 1 al 8)

--- jsEntradaSalida-4.js
+++ jsEntradaSalida-4.js
@@ -1,14 +1,12 @@
-/*
-	Debemos lograr tomar un dato por 'PROMPT'
+/* 
+	Debemos lograr tomar un dato por 'PROMPT' 
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-    var dato;
-    dato = prompt("ingrese dato");
-    document.getElementById("elNombre").value = dato;
-
-    //alert(dato);
+	var dato;
+dato = prompt("ingrese un dato");
+document.getElementById("elNombre").value = dato;
 
 }
 
