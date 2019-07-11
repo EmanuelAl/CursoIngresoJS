59c229021062f97ae0d74895be7b2cf5133a046b
EmanuelAl <52534676+EmanuelAl@users.noreply.github.com>
2019-07-11 11:20:47 -0300

    js.parcial1al3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,16 @@
 function mostrar()
 {
+var precio;
+var porcentaje;
+var resultado;
 
+precio = prompt("ingrese el precio ");
+porcentaje = prompt("ingrese el porcentaje de descuento ");
+precio = parseInt(precio);
+porcentaje= parseInt(porcentaje);
+
+resultado=(precio*porcentaje/100);
+resultado=(precio - resultado);
+
+document.getElementById("elPrecioFinal").value = resultado;
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,15 @@
 
 function mostrar()
 {
+    var ancho;
+    var largo;
+    var perimetro;
+    ancho = prompt(" ingrese el ancho");
+    largo = prompt(" ingrese el largo");
+    ancho= parseInt(ancho);
+    largo= parseInt(largo);
+
+    perimetro = (ancho + largo)* 2;
+    alert("El perimetro es :" + perimetro);
 
 }
