26f86316c39a787e693aa7e7895eab1ed68d8f15
EmanuelAl <emanuel-alderete@hotmail.com>
2019-07-28 23:43:32 -0300

    ej9-10While

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -3,15 +3,49 @@ function mostrar()
 
 	var contador=0;
 	//declarar contadores y variables 
-	
-	var respuesta="si";
-
-	while(respuesta!="no")
+	var sumNeg=0;
+	var sumPos=0;
+	var contN=0;
+	var contP=0;
+	contCero=0;
+	var resto=0;
+	var respuesta=true;
+	var contPares=0;
+	var promedioPos=0;
+	var promedioneg=0;
+	var difPosNeg=0;
+	while(respuesta)
 	{
-		
-	
-	}
+		do{
+			num=prompt("ingrese numero");
+			num=parseInt(num);
+		}while(isNaN(num));
 
+		if(num<0){
+			sumNeg+=num;
+			contN++;
+		}else {
+			if(num>0){
+			sumPos+=num;
+			contP++;
+			}else{
+				contCero++;
+			}
+		}
+		resto=num%2;
+		if (resto==0){
+			contPares++;
+		}
+		respuesta=confirm("quiere seguir ?");//devuelve true si damos "aceptar"
+	}
+	promedioPos=sumPos/contP;
+	promedioneg=sumNeg/contN;
+	difPosNeg=contP-contN;
+	document.write("<p> suma de negativos "+ sumNeg +"<br> suma de positivos "+sumPos+
+	"<br> cantidad de positivos "+contP+"<br> cantidad de negativos "+contN+"<br> cantidad de ceros "+
+	contCero+"<br> cantidad de pares "+contPares+"<br> promedio de positivos "+promedioPos+
+	"<br> promedio de negativos "+promedioneg+"<br> diferencia entre positivos y negativos"+
+	difPosNeg+"</p>");
 
 
 
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,16 +3,32 @@ function mostrar()
 
 	var contador=0;
 	// declarar variables
-	
-	var respuesta='si';
+	var num=0;
+	var max=0;
+	var min=0;
+	var bandera=true;
+	var respuesta=true;
 
-	while(respuesta!='no')
+	while(respuesta)
 	{
-		
-	
+		do{
+			num = prompt("ingrese numero");
+			num =parseInt(num);
+	 	}while(isNaN(num));
+		if(bandera){
+			max=num;
+			min=num;
+			bandera=false;
+		}
+		if(num>max){
+			max=num;
+		}else if(num<min){
+				min=num;
+			}
+		respuesta=confirm("quiere seguir ?");//devuelve true si damos "aceptar"
+
 	}
-
-
-
+	document.getElementById('maximo').value=max;
+	document.getElementById('minimo').value=min;
 
 }//FIN DE LA FUNCIÓN
